320979215e7f10d1d4b1ab4f8477a213a6f49088
dsteblyuk@gmail.com <dsteblyuk@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-04 19:21:24 +0000

    Adding bower. Removing vendor files. Move www to app.

--- implementations/group5/frontend/Gruntfile.js
+++ implementations/group5/frontend/Gruntfile.js
@@ -1,11 +1,33 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    wiredep: {
+
+      task: {
+
+        // Point to the files that should be updated when
+        // you run `grunt wiredep`
+        src: [
+          'app/index.html',   // .html support...
+        ],
+
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+
+          // https://github.com/taptapship/wiredep#configuration
+          // devDependencies: true, // default is false
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-wiredep');
 
   // this would be run by typing "grunt test" on the command line
   // grunt.registerTask('test', ['jshint', 'qunit']);
 
   // the default task can be run just by typing "grunt" on the command line
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['wiredep']);
 };	
