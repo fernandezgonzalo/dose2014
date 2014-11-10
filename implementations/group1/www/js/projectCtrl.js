72ded3b250d9a680abe6469d0d6499d2e9232b93
eli.adova@abv.bg <eli.adova@abv.bg@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-10 19:05:37 +0000

    Routes for administration panel, dashboard and ranking added. ProjectCtrl and RequirementCtrl added.

---
+++ implementations/group1/www/js/projectCtrl.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('Demo')
+  .controller('ProjectCtrl', ['$scope', '$http', '$log',
+	function ($scope, $http, $log) {
+
+      $scope.projects = [];
+
+      var init = function() {
+		// Initialization function that gets the projects from the server
+      }();
+	  
+	  $scope.getUserProjects = function(userId) {
+		// Gets from the server all the projects for a user
+      }
+	  
+      $scope.createProject = function(name, description) {
+		// Creates a new project with certain name
+      }
+	  
+	  $scope.deleteProject = function(projectId) {
+		// Deletes a project with certain id (can be executed only by the project manager of the project)
+      }
+	  
+	  $scope.editProject = function(projectId, name, description) {
+		// Modidifies the name and the description of a project with certain id
+      }
+	  
+	  $scope.addUser = function(projectId, userId) {
+		// Invites a user to a project (can be executed only by the project manager and the team leader of the project)
+      }
+	  
+	  $scope.removeUser = function(projectId, userId) {
+		// Removes a user from a project (can be executed only by the project manager and the team leader of the project)
+      }
+	  
+	  $scope.setTeamLeader = function(projectId, userId) {
+		// Makes a user the team leader of the project (can be executed only by the project manager of the project)
+      }
+	  
+	  $scope.unsetTeamLeader = function(projectId, userId) {
+		// Removes team leader role of the project (can be executed only by the project manager of the project)
+      }
+	  
+	  $scope.setIteration = function(projectId, iterationId) {
+		// Sets the current iteration of the project
+      }
+    }  
+  ]);
