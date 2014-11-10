e12a9a44323b65692f5891f14e3bf733b65cc2b3
palaciodelgado@gmail.com <palaciodelgado@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-10 23:21:20 +0000

    Merging between trunk and mi1-fe-project-pages. Adds the project pages to Trunk.

---
+++ trunk/implementations/group5/frontend/app/js/controllers/projects.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// var app = angular.module('Mgmt').controller('ProjectsController', ['Project', function(project) {}]);
+
+// angular.module('Mgmt').controller('ProjectsController',
+//                                   ['$scope', '$log', '$location', 'project',
+//                                     function ($scope, $log, $location, project) {
+                                      
+angular.module('Mgmt').controller('ProjectsController',
+                                  ['$scope', '$log', '$location', 'User',
+                                    function ($scope, $log, $location, User) {
+
+  $scope.users = User.query();
+
+  $scope.openProject = function() {
+    $location.path('/projects/1/dashboard');
+  };
+
+  $scope.login = function() {
+
+  };
+
+  // JavaScript object in order to ilustrate the template of /projects.html
+  this.inProgress = [
+    {
+      name: 'Distributed software development system',
+      client: 'DOSE 2014',
+      author: 'Martin',
+      date: 'Dec 15'
+    },
+    {
+      name: 'New webmail application',
+      client: 'Google',
+      author: 'Larry',
+      date: 'Jan 15'
+    },
+    {
+      name: 'Control software for spaceships',
+      client: 'Science Imp.',
+      author: 'Richard',
+      date: 'Feb 22'
+    }];
+
+  this.finished = [
+    {
+      name: 'Grammar recognizer',
+      client: 'Science Imp.',
+      author: 'Alan',
+      date: 'Oct 31'
+    },
+    {
+      name: 'Shopping mobile app development',
+      client: 'Amazon',
+      author: 'Jeff',
+      date: 'Aug 13'
+    }];
+}]);
+
