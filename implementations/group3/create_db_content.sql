a47da69e7449be86e8ae8b37e6fdb8d3dae269a1
onponomarev <onponomarev@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-11 22:38:54 +0000

    Database files updated

---
+++ implementations/group3/create_db_content.sql
@@ -0,0 +1,125 @@
+-- Drop tables if they exist
+DROP TABLE Users;
+DROP TABLE Sprints;
+DROP TABLE SprintComments;
+DROP TABLE Tasks;
+DROP TABLE TasksComments;
+DROP TABLE Projects;
+DROP TABLE ProjectsComments;
+DROP TABLE Stories;
+DROP TABLE StoriesComments;
+
+-- Make sure that foreign_key support is turned on
+PRAGMA foreign_keys = ON;
+
+-- At first, create tables
+
+CREATE TABLE Users (
+  id INTEGER PRIMARY KEY,
+  login TEXT NOT NULL UNIQUE,
+  password TEXT,
+  name TEXT
+);
+
+CREATE TABLE Sprints(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  start_time TEXT,
+  end_time TEXT
+);
+
+CREATE TABLE SprintComments(
+  id INTEGER PRIMARY KEY,
+  сontents TEXT NOT NULL UNIQUE,
+  date TEXT,
+  userId INTEGER NOT NULL,
+  sprintId INTEGER NOT NULL,
+  FOREIGN KEY(userId) REFERENCES Users(id),
+  FOREIGN KEY(sprintId) REFERENCES Sprints(id)
+);
+
+CREATE TABLE Tasks(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  points INTEGER,
+  status INTEGER,
+  completeness INTEGER
+);
+
+CREATE TABLE TasksComments(
+  id INTEGER PRIMARY KEY,
+  сontents TEXT NOT NULL UNIQUE,
+  date TEXT,
+  userId INTEGER NOT NULL,
+  taskId INTEGER NOT NULL,
+  FOREIGN KEY(userId) REFERENCES Users(id),
+  FOREIGN KEY(taskId) REFERENCES Tasks(id)
+);
+
+CREATE TABLE Projects(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE ProjectComments(
+  id INTEGER PRIMARY KEY,
+  сontents TEXT NOT NULL UNIQUE,
+  date TEXT,
+  userId INTEGER NOT NULL,
+  projectId INTEGER NOT NULL,
+  FOREIGN KEY(userId) REFERENCES Users(id),
+  FOREIGN KEY(projectId) REFERENCES Projects(id)
+);
+
+
+CREATE TABLE Stories(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE StoriesComments(
+  id INTEGER PRIMARY KEY,
+  сontents TEXT NOT NULL UNIQUE,
+  date TEXT,
+  userId INTEGER NOT NULL,
+  storyId INTEGER NOT NULL,
+  FOREIGN KEY(userId) REFERENCES Users(id),
+  FOREIGN KEY(storyId) REFERENCES Stories(id)
+);
+
+-- And now many to many relations
+
+CREATE TABLE StoriesBelongToProjects(
+  projectId INTEGER NOT NULL,
+  storyId INTEGER NOT NULL,
+  FOREIGN KEY(projectId) REFERENCES Projects(id) ON DELETE CASCADE,
+  FOREIGN KEY(storyId) REFERENCES Stories(id) ON DELETE CASCADE
+);
+
+CREATE TABLE SprintsBelongToProjects(
+  projectId INTEGER NOT NULL,
+  sprintId INTEGER NOT NULL,
+  FOREIGN KEY(projectId) REFERENCES Projects(id) ON DELETE CASCADE,
+  FOREIGN KEY(sprintId) REFERENCES Sprints(id) ON DELETE CASCADE
+);
+
+CREATE TABLE TasksBelongToStories(
+  taskId INTEGER NOT NULL,
+  storyId INTEGER NOT NULL,
+  FOREIGN KEY(taskId) REFERENCES Tasks(id) ON DELETE CASCADE,
+  FOREIGN KEY(storyId) REFERENCES Stories(id) ON DELETE CASCADE
+);
+
+CREATE TABLE TasksBelongToSprints(
+  taskId INTEGER NOT NULL,
+  sprintId INTEGER NOT NULL,
+  FOREIGN KEY(taskId) REFERENCES Tasks(id) ON DELETE CASCADE,
+  FOREIGN KEY(sprintId) REFERENCES Sprints(id) ON DELETE CASCADE
+);
+
+
+-- Add some users to the users table
+--INSERT INTO Users(name) VALUES ('John');
+
+-- Add some todos to the todos table
+--INSERT INTO Todos(description, userId) VALUES ('Be happy', 2);
