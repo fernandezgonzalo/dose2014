1964071fd036f2ac5029d16b2c645383cb2c2f2b
ludvig.stenstrom@gmail.com <ludvig.stenstrom@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-11 15:39:52 +0000

    Added new routes to app.js

--- implementations/group1/www/js/app.js
+++ implementations/group1/www/js/app.js
@@ -1,60 +1,68 @@
 'use strict';
 
 var app = angular.module('Demo', [
-  'ngRoute', 'ngResource'
+'ngRoute', 'ngResource'
 ]);
 
 /** Turn on/off the angular debugging; should be off when deployed */
 app.config(['$logProvider', function($logProvider){
-  $logProvider.debugEnabled(false);
-}]);
+	$logProvider.debugEnabled(false);
+	}]);
 
-/** Define the routes for the application; This routing is done by Angular */
-app.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/login', { 
-        templateUrl: 'partials/login.html',
-        controller: 'SessionCtrl'
-      })
-      .when('/user/:userID/logout', {
-        templateUrl: 'partials/logout.html',
-        controller: 'SessionCtrl'
-      })
-	  .when('/user/:userId/home', {
-        templateUrl: 'partials/home.html',
-        controller: 'ProjectCtrl'
-      })
-	  	  .when('/user/:userId/home/:projectId', {
-        templateUrl: 'partials/home.html',
-        controller: 'ProjectCtrl'
-	})
-	  .when('/user/:userId//cpanel', {
-        templateUrl: 'partials/cpanel.html',
-        controller: 'UserCtrl'
-      })
-	  .when('/ranking', {
-        templateUrl: 'partials/ranking.html',
-        controller: 'UserCtrl'
-	})
-	  .when('/user', {
-        templateUrl: 'partials/user.html',
-        controller: 'UserCtrl'
-	})
-	  .when('/user/:userId', {
-        templateUrl: 'partials/user.html',
-        controller: 'UserCtrl'
-	})
-	  .when('/signup', {
-        templateUrl: 'partials/user.html',
-        controller: 'UserCtrl'
-	})
-      .otherwise({
-        redirectTo: '/login'
-      });
+	/** Define the routes for the application; This routing is done by Angular */
+	app.config(['$routeProvider', '$locationProvider',
+	function($routeProvider, $locationProvider) {
+		$routeProvider
+		.when('/login', { 
+			templateUrl: 'partials/login.html',
+			controller: 'SessionCtrl'
+		})
+		.when('/user', {
+			templateUrl: 'partials/user.html',
+			controller: 'UserCtrl'
+		})
+		.when('/user/:userID/logout', {
+			templateUrl: 'partials/logout.html',
+			controller: 'SessionCtrl'
+		})
+		.when('/user/:userId/home', {
+			templateUrl: 'partials/home.html',
+			controller: 'ProjectCtrl'
+		})
+		.when('/user/:userId/home/:projectId', {
+			templateUrl: 'partials/home.html',
+			controller: 'ProjectCtrl'
+		})
+		.when('/user/:userId//cpanel', {
+			templateUrl: 'partials/cpanel.html',
+			controller: 'UserCtrl'
+		})
+		.when('/ranking', {
+			templateUrl: 'partials/ranking.html',
+			controller: 'UserCtrl'
+		})
+		.when('/user/:userId', {
+			templateUrl: 'partials/user.html',
+			controller: 'UserCtrl'
+		})
+		.when('/signup', {
+			templateUrl: 'partials/user.html',
+			controller: 'UserCtrl'
+		})
+		.when('/sprint', {
+	        templateUrl: 'partials/sprint.html',
+	        controller: 'SprintCtrl'
+		})
+		.when('/task', {
+	        templateUrl: 'partials/task.html',
+			controller: 'TaskCtrl'
+		})
+		.otherwise({
+			redirectTo: '/login'
+		});
     }
 	
-  ]);
+	]);
  
   
   
