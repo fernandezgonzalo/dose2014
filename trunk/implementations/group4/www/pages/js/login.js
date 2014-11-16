224b745626c4e9e61e7b2f4e24e5ca6b8a0befd4
andrew.d.lapin@gmail.com <andrew.d.lapin@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-16 22:20:19 +0000

    Added restapi provider

--- trunk/implementations/group4/www/pages/js/login.js
+++ trunk/implementations/group4/www/pages/js/login.js
@@ -5,7 +5,8 @@ define(
         "angular",
 
         //Custom includes
-        "blocks/registration/registration"
+        "blocks/registration/registration",
+        "pages/js/restapi"
     ],
 
     function(angular)
@@ -15,6 +16,7 @@ define(
         (
             "LoginModule",
             [
+                "RestApiModule",
                 "uiRegistrationModule"
             ]
         )
@@ -25,11 +27,19 @@ define(
             [
                 "$scope",
                 "$log",
-                function($scope, $log)
+                "restapi",
+                function($scope, $log, restapi)
                 {
                     $scope.login = function(form)
                     {
                         $log.info(form);
+                        restapi.login(form.email, form.password);
+                    }
+
+                    $scope.logout = function(form)
+                    {
+                        $log.info(form);
+                        restapi.logout();
                     }
                 }
             ]
