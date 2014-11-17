e7d7fbee74b218ad2d556189f80c3fe0d422ac3e
aaron.richiger@gmail.com <aaron.richiger@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-17 01:46:19 +0000

    Changed credentials in hello_world.py

--- implementations/group7/lets_go_project/backend_tests/hello_world.py
+++ implementations/group7/lets_go_project/backend_tests/hello_world.py
@@ -2,6 +2,6 @@ import config
 import requests
 
 
-cookie = requests.post('http://localhost:9090/sessions', data='{"username": "Mary", "password": "secret"}').cookies['lets_go_session']
-users = requests.get('http://localhost:9090/users/1', cookies=dict(lets_go_session=cookie))
-print users.text
+cookie = requests.post('http://localhost:9090/sessions', data='{"username": "asdf", "password": "asdf"}').cookies['lets_go_session']
+user = requests.get('http://localhost:9090/users/1', cookies=dict(lets_go_session=cookie))
+print user.text
