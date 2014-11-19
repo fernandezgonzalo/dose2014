7ef3673c824ecffe2b6a98fa02cd7302f0129c95
dsteblyuk@gmail.com <dsteblyuk@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-19 23:52:58 +0000

    Group5: implemented user editing.

--- implementations/group5/frontend/app/js/services/user.js
+++ implementations/group5/frontend/app/js/services/user.js
@@ -1,8 +1,17 @@
 'use strict';
 
-angular.module('Mgmt').factory('User', ['$resource', function($resource) {  
+angular.module('Mgmt').factory('User', ['$resource', '$http', '$log', function($resource, $http, $log) {  
+
+  var TAG = 'UserService::';
+
+  $log.debug(TAG, 'init');
 
   var User = $resource('/api/users/:userId', {userId: '@id'});
+
+  User.prototype.$update = function(success, error) {
+    $http.put('/api/users/' + this.id, this).then(success, error);
+  };
+
   return User;
 
 }]);
