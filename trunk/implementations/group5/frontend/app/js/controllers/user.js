c54ef6f1fe28d3dfe52270c9c44b911221cf3c8c
dsteblyuk@gmail.com <dsteblyuk@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-19 23:52:58 +0000

    Group5: implemented user editing.

--- trunk/implementations/group5/frontend/app/js/controllers/user.js
+++ trunk/implementations/group5/frontend/app/js/controllers/user.js
@@ -3,6 +3,8 @@
 angular.module('Mgmt').controller('UserController', ['$scope', '$log', '$location', '$routeParams', '$resource', 'Utility', 'User', function ($scope, $log, $location, $routeParams, $resource, Utility, User) {
   var TAG = 'UserController::';
 
+  $scope.userSaved = false;
+
   $log.debug(TAG, 'init', $routeParams);
   if ($routeParams.id) {
     $scope.isEdit = true;
@@ -31,7 +33,41 @@ angular.module('Mgmt').controller('UserController', ['$scope', '$log', '$locatio
     $event.stopPropagation();
   };
 
-  $scope.create = function() {
+  $scope.submit = function() {
+    if ($scope.isEdit) {
+      save();
+    } else {
+      create();
+    }
+  };
+
+  function animate() {
+    $scope.saveSucceed = true;
+    setTimeout(function() {
+      $scope.hiding = true;
+      $scope.$apply();
+      setTimeout(function() {
+        $scope.saveSucceed = false;
+        $scope.hiding = false;
+        $scope.$apply();
+      }, 3000);
+    }, 7000);
+  }
+
+  $scope.hide = function() {
+    $scope.showSuccess = false;
+  };
+
+  function save() {
+    var user = $scope.user;
+    user.$update(function() {
+      animate();
+    }, function() {
+      $scope.saveUserError = true;
+    });
+  }
+
+  function create() {
     var user = $scope.user;
     if (!user) {
       user = {};
@@ -44,5 +80,5 @@ angular.module('Mgmt').controller('UserController', ['$scope', '$log', '$locatio
     }, function() {
       $scope.createUserError = true;
     });
-  };
+  }
 }]);
