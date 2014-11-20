2a46c682e1cca7b52974468c2ad6702e6aae6133
andrew.d.lapin@gmail.com <andrew.d.lapin@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-20 19:28:15 +0000

    Added projects resolver

--- trunk/implementations/group4/www/pages/js/projects.js
+++ trunk/implementations/group4/www/pages/js/projects.js
@@ -49,7 +49,32 @@ define(
                     var module = {};
                     module.resolver = function()
                     {
-                        return ["project1", "project2", "project3", "project4"];
+                        return [
+                            {
+                                "id":"1",
+                                "name":"project1",
+                                "status":"Active",
+                                "description":"project1 descr",
+                                "max_points_per_sprint":"12",
+                                "number_of_sprints":"3"
+                            },
+                            {
+                                "id":"2",
+                                "name":"project2",
+                                "status":"Active",
+                                "description":"project2 descr",
+                                "max_points_per_sprint":"15",
+                                "number_of_sprints":"1"
+                            },
+                            {
+                                "id":"3",
+                                "name":"project3",
+                                "status":"Active",
+                                "description":"project3 descr",
+                                "max_points_per_sprint":"16",
+                                "number_of_sprints":"1"
+                            }
+                        ];
                     };
                     return module;
                 }
