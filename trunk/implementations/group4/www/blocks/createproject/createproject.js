67dbb8c4d92979c5c02dbe858db2f87ef2c2aa07
andrew.d.lapin@gmail.com <andrew.d.lapin@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-21 12:55:24 +0000

    Added modals to project page

---
+++ trunk/implementations/group4/www/blocks/createproject/createproject.js
@@ -0,0 +1,75 @@
+
+define(
+    [
+        //System includes
+        "angular",
+        "bootstrapUi",
+        "angularUtils",
+    ],
+
+    function (angular)
+    {
+        return angular.module
+        (
+            "uiCreateProjectModule",
+            [
+                "RestApiModule",
+                "ui.bootstrap",
+                "ui.validate"
+            ]
+        )
+
+        .controller
+        (
+            'ModalCtr',
+            [
+                "$scope",
+                "$modalInstance",
+                function ($scope, $modalInstance)
+                {
+                    $scope.save = function (form)
+                    {
+                        $modalInstance.close(form);
+                    };
+
+                    $scope.cancel = function ()
+                    {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }
+            ]
+        )
+
+        .controller
+        (
+            "CreateProjectCtr",
+            [
+                "$scope",
+                "$modal",
+                "$log",
+                function($scope, $modal, $log)
+                {
+                    $scope.createProject = function (size)
+                    {
+                        var modalInstance = $modal.open
+                        (
+                            {
+                                templateUrl: 'blocks/createproject/modal.html',
+                                controller: 'ModalCtr',
+                                size: size
+                            }
+                        );
+
+                        modalInstance.result.then
+                        (
+                            function (form)
+                            {
+                                $log.info(form);
+                            }
+                        );
+                    };
+                }
+            ]
+        );
+    }
+);
---
+++ trunk/implementations/group4/www/blocks/editproject/editproject.js
@@ -0,0 +1,75 @@
+
+define(
+    [
+        //System includes
+        "angular",
+        "bootstrapUi",
+        "angularUtils",
+    ],
+
+    function (angular)
+    {
+        return angular.module
+        (
+            "uiEditProjectModule",
+            [
+                "RestApiModule",
+                "ui.bootstrap",
+                "ui.validate"
+            ]
+        )
+
+        .controller
+        (
+            'ModalCtr',
+            [
+                "$scope",
+                "$modalInstance",
+                function ($scope, $modalInstance)
+                {
+                    $scope.save = function (form)
+                    {
+                        $modalInstance.close(form);
+                    };
+
+                    $scope.cancel = function ()
+                    {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }
+            ]
+        )
+
+        .controller
+        (
+            "EditProjectCtr",
+            [
+                "$scope",
+                "$modal",
+                "$log",
+                function($scope, $modal, $log)
+                {
+                    $scope.editProject = function (size)
+                    {
+                        var modalInstance = $modal.open
+                        (
+                            {
+                                templateUrl: 'blocks/editproject/modal.html',
+                                controller: 'ModalCtr',
+                                size: size
+                            }
+                        );
+
+                        modalInstance.result.then
+                        (
+                            function (form)
+                            {
+                                $log.info(form);
+                            }
+                        );
+                    };
+                }
+            ]
+        );
+    }
+);
