1b4b8cefe90881957e91781d6a9e95ff98281b9a
joaquintita@gmail.com <joaquintita@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-22 16:28:07 +0000

    updated online version with local version

--- implementations/group7/lets_go_project/www/js/app.js
+++ implementations/group7/lets_go_project/www/js/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var app = angular.module('Demo', [
-  'ngRoute'
-]);
+var app = angular.module('myApp', ['ngRoute', 'ngCookies', 'ui.bootstrap']);
 
 /** Turn on/off the angular debugging; should be off when deployed */
 app.config(['$logProvider', function($logProvider){
@@ -13,9 +11,9 @@ app.config(['$logProvider', function($logProvider){
 app.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider
-      .when('/todos', {
-        templateUrl: 'partials/todos.html',
-        controller: 'TodosCtrl'
+      .when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl'
       })
       .when('/login', {
         templateUrl: 'partials/login.html',
@@ -25,8 +23,71 @@ app.config(['$routeProvider', '$locationProvider',
         templateUrl: 'partials/logout.html',
         controller: 'SessionCtrl'
       })
+      .when('/users/', {
+        templateUrl: 'partials/user.html',
+        controller: 'UsersCtrl'
+      })
+      .when('/project/create', {
+        templateUrl: 'partials/create_project.html',
+        controller: 'ProjectsCtrl'
+      })
+      .when('/project/edit/:projectId', {
+        templateUrl: 'partials/edit_project.html',
+        controller: 'ProjectsCtrl'
+      })
+      .when('/projects', {
+        templateUrl: 'partials/projects.html',
+        controller: 'ProjectsCtrl'
+      })
+
+      .when('/requirements', {
+        templateUrl: 'partials/requirements.html',
+        controller: 'RequirementsCtrl'
+      })
+      .when('/sprints', {
+        templateUrl: 'partials/sprints.html',
+        controller: 'SprintsCtrl'
+      })
+      .when('/tasks', {
+        templateUrl: 'partials/tasks.html',
+        controller: 'TasksCtrl'
+      })
       .otherwise({
-        redirectTo: '/todos'
+        redirectTo: '/login'
       });
     }
   ]);
+
+app.run(function ($rootScope, $location, $http, $timeout, AuthService, RESTService) {
+
+
+  $rootScope.authService = AuthService;
+  $rootScope.restService = RESTService;
+
+  $rootScope.$watch('authService.authorized()', function () {
+
+    // if never logged in, do nothing (otherwise bookmarks fail)
+    if ($rootScope.authService.initialState()) {
+      // we are public browsing
+      return;
+    }
+
+    // instantiate and initialize an auth notification manager
+    //  $rootScope.authNotifier = new NotificationManager($rootScope);
+
+    // when user logs in, redirect to home
+    if ($rootScope.authService.authorized()) {
+      $location.path("/projects");
+      //  $rootScope.authNotifier.notify('information', 'Welcome ' + $rootScope.authService.currentUser() + "!");
+    }
+
+    // when user logs out, redirect to home
+    if (!$rootScope.authService.authorized()) {
+      $location.path("/");
+      //$rootScope.authNotifier.notify('information', 'Thanks for visiting.  You have been signed out.');
+    }
+
+  }, true);
+
+
+});
