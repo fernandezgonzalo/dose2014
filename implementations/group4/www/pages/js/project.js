6b9a4905230f66d7c020b2c339e96f51cf93ecd2
andrew.d.lapin@gmail.com <andrew.d.lapin@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-24 23:36:37 +0000

    Added new project specific pages

---
+++ implementations/group4/www/pages/js/project.js
@@ -0,0 +1,94 @@
+
+define(
+    [
+        //System includes
+        "angular",
+
+        //Custom includes
+    ],
+
+    function(angular)
+    {
+        return angular.module
+        (
+            "ProjectModule",
+            [
+            ]
+        )
+
+        .controller
+        (
+            "ProjectCtr",
+            [
+                "$scope",
+                "$state",
+                function($scope, $state)
+                {
+                    $scope.state = $state;
+                }
+            ]
+        )
+
+        .controller
+        (
+            "ProjectSprintsCtr",
+            [
+                "$scope",
+                function($scope)
+                {
+                    $scope.add_sprint = function()
+                    {
+
+                    }
+                }
+            ]
+        )
+
+        .controller
+        (
+            "ProjectTasksCtr",
+            [
+                "$scope",
+                function($scope)
+                {
+                    $scope.add_task = function()
+                    {
+
+                    }
+                }
+            ]
+        )
+
+        .controller
+        (
+            "ProjectUsersCtr",
+            [
+                "$scope",
+                function($scope)
+                {
+                    $scope.users =
+                    [
+                        {
+                            "user_name":"name1",
+                            "email":"mail"
+                        },
+                        {
+                            "user_name":"name2",
+                            "email":"mail"
+                        }
+                    ];
+
+                    $scope.query = "";
+
+                    $scope.data = [{"id":"1","user_name":"name","is_active":"1","email":"name1@mail.com"},{"id":"2","user_name":"name1","is_active":"1","email":"name2@mail.com"},{"id":"3","user_name":"name2","is_active":"0","email":"name3@mail.com"},{"id":"4","user_name":"Foo","is_active":"1","email":"foo@foo.foo"}];
+
+                    $scope.add_user = function()
+                    {
+
+                    }
+                }
+            ]
+        )
+
+    }
+);
