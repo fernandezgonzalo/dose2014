ad2721c5f7af60a07428729c1ef00181b133140f
gabriele.madd@gmail.com <gabriele.madd@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-24 19:44:21 +0000

    now new task from user dashboard works. task crud for prototype completed

--- implementations/group5/frontend/app/js/services/task.js
+++ implementations/group5/frontend/app/js/services/task.js
@@ -3,16 +3,10 @@
 angular.module('Mgmt').factory('Task', ['$resource', '$log', function($resource, $log) {  
 
   var TAG = 'Task::';
-  var Task = $resource('/api/tasks/:taskId', {taskId: '@id'}, {'update':   {method: 'PUT'}});
-
-  /*
-  Task.prototype.$update = function(success, error) {
-  	$http.put('/api/tasks/' + this.id, this).then(success, error);
-  };
-	*/
-
   $log.debug(TAG, 'init');
 
+  var Task = $resource('/api/tasks/:taskId', {taskId: '@id'}, {'update':   {method: 'PUT'}});
+  
   return Task;
 
 }]);
