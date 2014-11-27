929531bd7ba04a10e046864367d43b5daccb6675
dsteblyuk@gmail.com <dsteblyuk@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-27 15:06:57 +0000

    Group5: modified development app deploy script.

--- implementations/group5/deploy/dev-backend-deploy.sh
+++ implementations/group5/deploy/dev-backend-deploy.sh
@@ -2,9 +2,12 @@
 pushd /home/dose/dose2014
 pushd backend
 svn update
-ec -config mgmt.ecf -target mgmt_cgi -finalize -c_compile -project_path .
-cp EIFGENs/mgmt_cgi/F_code/mgmt .
-mv mgmt mgmt.cgi
-cp mgmt.cgi /usr/lib/cgi-bin/
+sed -i '/-- sed replace db/{N; s/Result.*/Result := "\/var\/www\/db\/test.db"/}' application.e
+sed -i '/-- sed replace www/{N; s/Result.*/Result := "\/var\/www\/html"/}' application.e
+ec -config mgmt.ecf -target mgmt_cgi
+pushd EIFGENs/mgmt_cgi/W_code
+finish_freezing
+mv ./mgmt /usr/lib/cgi-bin/mgmt.cgi
+popd
 popd
 popd
