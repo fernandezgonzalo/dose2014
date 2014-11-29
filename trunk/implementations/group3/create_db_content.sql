407ddf3b89cc20bfe4696f0a54e2f00e38cc20fd
onponomarev <onponomarev@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-11-29 21:33:38 +0000

    DB fixed

--- trunk/implementations/group3/create_db_content.sql
+++ trunk/implementations/group3/create_db_content.sql
@@ -9,9 +9,10 @@ DROP TABLE ProjectComments;
 DROP TABLE Stories;
 DROP TABLE StoriesComments;
 
-DROP TABLE StoriesBelongToProjects;
-DROP TABLE TasksBelongToStories;
-DROP TABLE TasksBelongToSprints;
+DROP TABLE UsersToProjects;
+DROP TABLE UsersToSprints;
+DROP TABLE UsersToStories;
+DROP TABLE UsersToTasks;
 
 -- Make sure that foreign_key support is turned on
 PRAGMA foreign_keys = ON;
@@ -20,10 +21,9 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE Users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  login TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
   password TEXT,
-  name TEXT,
-  email TEXT
+  name TEXT
 );
 
 CREATE TABLE Projects(
@@ -40,7 +40,6 @@ CREATE TABLE ProjectComments(
   FOREIGN KEY(userId) REFERENCES Users(id),
   FOREIGN KEY(projectId) REFERENCES Projects(id)
 );
-
 
 CREATE TABLE Sprints(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -61,27 +60,11 @@ CREATE TABLE SprintComments(
   FOREIGN KEY(sprintId) REFERENCES Sprints(id)
 );
 
-CREATE TABLE Tasks(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL UNIQUE,
-  points INTEGER,
-  status INTEGER,
-  completeness INTEGER
-);
-
-CREATE TABLE TasksComments(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  сontents TEXT NOT NULL UNIQUE,
-  date TEXT,
-  userId INTEGER NOT NULL,
-  taskId INTEGER NOT NULL,
-  FOREIGN KEY(userId) REFERENCES Users(id),
-  FOREIGN KEY(taskId) REFERENCES Tasks(id)
-);
-
 CREATE TABLE Stories(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL UNIQUE
+  name TEXT NOT NULL UNIQUE,
+  sprintId INTEGER NOT NULL,
+  FOREIGN KEY(sprintId) REFERENCES Sprints(id)
 );
 
 CREATE TABLE StoriesComments(
@@ -94,29 +77,60 @@ CREATE TABLE StoriesComments(
   FOREIGN KEY(storyId) REFERENCES Stories(id)
 );
 
+CREATE TABLE Tasks(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE,
+  storyId INTEGER NOT NULL,
+  points INTEGER,
+  status INTEGER,
+  completeness INTEGER,
+  FOREIGN KEY(storyId) REFERENCES Stories(id)
+);
+
+CREATE TABLE TasksComments(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  сontents TEXT NOT NULL UNIQUE,
+  date TEXT,
+  userId INTEGER NOT NULL,
+  taskId INTEGER NOT NULL,
+  FOREIGN KEY(userId) REFERENCES Users(id),
+  FOREIGN KEY(taskId) REFERENCES Tasks(id)
+);
+
+
 -- And now many to many relations
 
-CREATE TABLE StoriesBelongToProjects(
+CREATE TABLE UsersToProjects(
   projectId INTEGER NOT NULL,
-  storyId INTEGER NOT NULL,
+  userId INTEGER NOT NULL,
+  role INTEGER NOT NULL,
   FOREIGN KEY(projectId) REFERENCES Projects(id) ON DELETE CASCADE,
-  FOREIGN KEY(storyId) REFERENCES Stories(id) ON DELETE CASCADE
+  FOREIGN KEY(userId) REFERENCES Users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE TasksBelongToStories(
-  taskId INTEGER NOT NULL,
-  storyId INTEGER NOT NULL,
-  FOREIGN KEY(taskId) REFERENCES Tasks(id) ON DELETE CASCADE,
-  FOREIGN KEY(storyId) REFERENCES Stories(id) ON DELETE CASCADE
-);
-
-CREATE TABLE TasksBelongToSprints(
-  taskId INTEGER NOT NULL,
+CREATE TABLE UsersToSprints(
   sprintId INTEGER NOT NULL,
-  FOREIGN KEY(taskId) REFERENCES Tasks(id) ON DELETE CASCADE,
-  FOREIGN KEY(sprintId) REFERENCES Sprints(id) ON DELETE CASCADE
+  userId INTEGER NOT NULL,
+  role INTEGER NOT NULL,
+  FOREIGN KEY(sprintId) REFERENCES Sprint(id) ON DELETE CASCADE,
+  FOREIGN KEY(userId) REFERENCES Users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE UsersToStories(
+  storyId INTEGER NOT NULL,
+  userId INTEGER NOT NULL,
+  role INTEGER NOT NULL,
+  FOREIGN KEY(storyId) REFERENCES Story(id) ON DELETE CASCADE,
+  FOREIGN KEY(userId) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE UsersToTasks(
+  taskId INTEGER NOT NULL,
+  userId INTEGER NOT NULL,
+  role INTEGER NOT NULL,
+  FOREIGN KEY(taskId) REFERENCES Tasks(id) ON DELETE CASCADE,
+  FOREIGN KEY(userId) REFERENCES Users(id) ON DELETE CASCADE
+);
 
 -- Add some users to the users table
 --INSERT INTO Users(name) VALUES ('John');
