3aa8a33a2e19836e469f4389fd059fb5e32e3617
federico@reghe.net <federico@reghe.net@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-02 10:53:43 +0000

    [group8] resolved conflict, /account/ now full working

--- trunk/implementations/group8/src/tests/apis/developerslist.py
+++ trunk/implementations/group8/src/tests/apis/developerslist.py
@@ -6,7 +6,9 @@ from suite_functions import check_reply
 params = "";
 
 expected_response = json.loads("""
-{"developers":[{"id":1,"firstname":"Filippo","lastname":"Pagano"},{"id":2,"firstname":"Federico","lastname":"Reghenzani"},{"id":5,"firstname":"Federico","lastname":"Reghenzani"}]}
+{"developers": [{"id": 1, "lastname": "Pagano", "firstname": "Filippo"}, 
+{"id": 2, "lastname": "Reghenzani", "firstname": "Federico"}, {"id": 5, 
+"lastname": "Reghenzani", "firstname": "Federico"}]}
 """)
 
 def exec_test(debug=False):
