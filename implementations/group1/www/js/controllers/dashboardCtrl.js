081420f8fcf07a058e56b41c9574f25d103c66d6
ludvig.stenstrom@gmail.com <ludvig.stenstrom@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-02 11:30:56 +0000

    Group1 - Create basic userDashboard woth links to projetDasboard

--- implementations/group1/www/js/controllers/dashboardCtrl.js
+++ implementations/group1/www/js/controllers/dashboardCtrl.js
@@ -6,9 +6,12 @@
 
 angular.module('DOSEMS.controllers')
     .controller('DashboardCtrl', ['$scope', '$routeParams', '$log', 'Users', 'Projects', '$window', function ($scope, $routeParams, $log, Users, Projects, $window) {
-        var userId = $routeParams.userId;
-        $log.debug("RouteParam = " + userId);
         $scope.init = function () {
+            $scope.userId = $routeParams.userId;
+            $log.debug("RouteParam = " + $scope.userId);
+            $scope.user = Users.currentUser;
+            $log.debug("Dashboard - CurrentUser: ");
+            $log.debug($scope.user);
             if (!Users.loggedIn) {
                 $window.location.href = '/#/login';
             }
@@ -17,7 +20,7 @@ angular.module('DOSEMS.controllers')
         };
         $scope.getUserProjectsIDs = function () {
             //Get the users projects from server
-            var response = Projects.get({userId: userId});
+            var response = Projects.get({userId: $scope.userId});
             response.$promise.then(function (data) {
                 $log.debug("Dashboard - Getting userProjectIds");
                 $log.debug(data);
@@ -27,8 +30,15 @@ angular.module('DOSEMS.controllers')
         };
         $scope.getUserProjects = function (userProjectIDs) {
             $log.debug("Dashboard - Getting userProjects");
+            $scope.userProjects = [];
             angular.forEach(userProjectIDs, function (obj, id) {
                 $log.debug(obj.id_project + " " + obj.id_user);
+                var response = Projects.get({userId: obj.id_user, projectId: obj.id_project});
+                response.$promise.then(function (data) {
+                    $log.debug("Printing project data");
+                    $log.debug(data);
+                    $scope.userProjects.push(data[0])
+                });
             });
 
         };
