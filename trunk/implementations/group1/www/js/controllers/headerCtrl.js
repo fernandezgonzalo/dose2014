fa2d2a317157c49370b6f437c0269f173ce7182b
ludvig.stenstrom@gmail.com <ludvig.stenstrom@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-02 18:11:10 +0000

    Group1 - Bugfix in headerCtrl

--- trunk/implementations/group1/www/js/controllers/headerCtrl.js
+++ trunk/implementations/group1/www/js/controllers/headerCtrl.js
@@ -8,10 +8,12 @@ angular.module('DOSEMS.controllers')
     .controller('HeaderCtrl', ['Users', '$scope', '$log', '$cookieStore', function (Users, $scope, $log, $cookieStore) {
         $scope.init = function () {
             $scope.loggedIn = $cookieStore.get('loggedIn');
-            $scope.userId = $cookieStore.get('userId');
-            Users.resource.get({userId: $scope.userId}).$promise.then(function (data) {
-                $scope.user = data[0];
-            });
+            if ($scope.loggedIn) {
+                $scope.userId = $cookieStore.get('userId');
+                Users.resource.get({userId: $scope.userId}).$promise.then(function (data) {
+                    $scope.user = data[0];
+                });
+            }
             $scope.$on('loggedIn', function (event, data) {
                 $scope.loggedIn = true;
                 $log.debug("Responded to the loggedIn event");
