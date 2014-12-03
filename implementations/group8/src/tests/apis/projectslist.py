22d21a218d4933300ed464e0921bbf21a6fea5f4
federico@reghe.net <federico@reghe.net@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-03 20:53:09 +0000

    Fixed DB incoehrence / fixed tests

--- implementations/group8/src/tests/apis/projectslist.py
+++ implementations/group8/src/tests/apis/projectslist.py
@@ -3,15 +3,16 @@ import json, login
 
 from suite_functions import check_reply
 
-params = "1";
+params = "";
 
 expected_response = json.loads("""
-{"projects":[{"id":1,"name":"DOSE"}]}
+{"projects":[{"id":1,"name":"DOSE","description":"Create DOSE 
+software","manager":2,"stakeholder":3,"creationDate":1415555014,"deleted":false,"developers":[1]}]}
 """)
 
 def exec_test(debug=False):
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Cookie" : "_pdt_session_id_="+login.cookie_id+""}
     conn = http.client.HTTPConnection("localhost", 8080)
-    conn.request("GET", "/project/listprojects", params, headers)
+    conn.request("GET", "/projects/list", params, headers)
 
     return check_reply(conn, expected_response, debug)
