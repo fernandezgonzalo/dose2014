59d1dd9fe19d8becdc874f8bf5ed272e577e5ba7
federico@reghe.net <federico@reghe.net@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-04 22:21:27 +0000

    Add tests and query for statistics

---
+++ trunk/implementations/group8/src/tests/apis/tasksedit.py
@@ -0,0 +1,24 @@
+import http.client
+import json, login
+
+from suite_functions import check_reply
+
+params = """{
+"name" : "A created task!",
+"description" : "A description for this edited task!",
+"points" : 34,
+"developer" : 1,
+"state" : "Completed",
+"pbi" : 5
+}""";
+
+expected_response = json.loads("""
+{"status":"ok"}
+""")
+
+def exec_test(debug=False):
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Cookie" : "_pdt_session_id_="+login.cookie_id+""}
+    conn = http.client.HTTPConnection("localhost", 8080)
+    conn.request("POST", "/projects/1/pbis/2/tasks/1/edit", params, headers)
+
+    return check_reply(conn, expected_response, debug)
