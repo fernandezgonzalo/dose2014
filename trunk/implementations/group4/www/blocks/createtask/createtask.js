ac81783b72626bb102fd3e7354649b34fedb688c
andrew.d.lapin@gmail.com <andrew.d.lapin@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-04 21:21:43 +0000

    Added create task modal window

---
+++ trunk/implementations/group4/www/blocks/createtask/createtask.js
@@ -0,0 +1,130 @@
+
+define(
+    [
+        //System includes
+        "angular",
+        "bootstrapUi"
+    ],
+
+    function (angular)
+    {
+        return angular.module
+        (
+            "uiCreateTaskModule",
+            [
+                "ui.bootstrap"
+            ]
+        )
+
+        .controller
+        (
+            'CreateTaskModalCtr',
+            [
+                "$scope",
+                "$modalInstance",
+                function ($scope, $modalInstance)
+                {
+                    $scope.type_opt =
+                    [
+                        {
+                            id: "Bug",
+                            name: "Bug"
+                        },
+                        {
+                            id: "Feature",
+                            name: "Feature"
+                        }
+                    ];
+
+                    $scope.priority_opt =
+                    [
+                        {
+                            id: "Normal",
+                            name: "Normal"
+                        },
+                        {
+                            id: "High",
+                            name: "High"
+                        },
+                        {
+                            id: "Low",
+                            name: "Low"
+                        }
+                    ];
+
+                    $scope.position_opt =
+                    [
+                        {
+                            id: "Backlog",
+                            name: "Backlog"
+                        },
+                        {
+                            id: "Process",
+                            name: "Process"
+                        },
+                        {
+                            id: "Done",
+                            name: "Done"
+                        },
+                        {
+                            id: "Testing",
+                            name: "Testing"
+                        },
+                        {
+                            id: "Canceled",
+                            name: "Canceled"
+                        }
+                    ];
+
+                    $scope.form =
+                    {
+                        type: "Feature",
+                        priority: "Normal",
+                        position: "Backlog"
+                    };
+
+                    $scope.save = function (form)
+                    {
+                        $modalInstance.close(form);
+                    };
+
+                    $scope.cancel = function ()
+                    {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }
+            ]
+        )
+
+        .controller
+        (
+            "CreateTaskCtr",
+            [
+                "$scope",
+                "$modal",
+                function($scope, $modal)
+                {
+                    $scope.createTask = function (size)
+                    {
+                        var modalInstance = $modal.open
+                        (
+                            {
+                                templateUrl: 'blocks/createtask/modal.html',
+                                controller: 'CreateTaskModalCtr',
+                                size: size
+                            }
+                        );
+
+                        modalInstance.result.then
+                        (
+                            function (form)
+                            {
+                                $scope.$emit("create_task", form);
+                            }
+                        );
+                    };
+                }
+            ]
+        );
+    }
+);
---
+++ trunk/implementations/group4/www/blocks/edittask/edittask.js
@@ -0,0 +1,3 @@
+/**
+ * Created by andrew on 04.12.14.
+ */
