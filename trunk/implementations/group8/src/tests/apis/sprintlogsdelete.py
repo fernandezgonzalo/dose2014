b85c7db1eac60d0b326cb08008b6962c06a4763f
federico@reghe.net <federico@reghe.net@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-05 20:27:42 +0000

    Added some testes

--- trunk/implementations/group8/src/tests/apis/sprintlogsdelete.py
+++ trunk/implementations/group8/src/tests/apis/sprintlogsdelete.py
@@ -4,22 +4,15 @@ import json, login
 from suite_functions import check_reply
 
 params = """{
-
-"name" : "",
-"description" : "Questa e una prova",
-"startDate" : 1234567,
-"idpbis" : [1,2],
-"endDate" : 1852495723
-
 }""";
 
 expected_response = json.loads("""
-{"status":"error","field":"name","reason":"Name not present or not correct."}
+{"status":"ok"}
 """)
 
 def exec_test(debug=False):
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Cookie" : "_pdt_session_id_="+login.cookie_id+""}
     conn = http.client.HTTPConnection("localhost", 8080)
-    conn.request("POST", "/projects/1/sprintlogs/create", params, headers)
+    conn.request("POST", "/projects/1/sprintlogs/2/delete", params, headers)
 
     return check_reply(conn, expected_response, debug)
--- trunk/implementations/group8/src/tests/apis/sprintlogslistpbis.py
+++ trunk/implementations/group8/src/tests/apis/sprintlogslistpbis.py
@@ -4,22 +4,15 @@ import json, login
 from suite_functions import check_reply
 
 params = """{
-
-"name" : "",
-"description" : "Questa e una prova",
-"startDate" : 1234567,
-"idpbis" : [1,2],
-"endDate" : 1852495723
-
 }""";
 
 expected_response = json.loads("""
-{"status":"error","field":"name","reason":"Name not present or not correct."}
+{"pbis":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]}
 """)
 
 def exec_test(debug=False):
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Cookie" : "_pdt_session_id_="+login.cookie_id+""}
     conn = http.client.HTTPConnection("localhost", 8080)
-    conn.request("POST", "/projects/1/sprintlogs/create", params, headers)
+    conn.request("GET", "/projects/1/sprintlogs/1/listpbis", params, headers)
 
     return check_reply(conn, expected_response, debug)
