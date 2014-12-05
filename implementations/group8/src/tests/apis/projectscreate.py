721fd51331e222a69d089892d1c2cba956d36b94
mikelv92@gmail.com <mikelv92@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-05 15:43:55 +0000

    test backlog

--- implementations/group8/src/tests/apis/projectscreate.py
+++ implementations/group8/src/tests/apis/projectscreate.py
@@ -1,12 +1,12 @@
 import http.client
-import json, login
+import json, login_sh
 
 from suite_functions import check_reply
 
 params = """{
 "name" : "7es7 pr0j3c7",
 "description" : "Qu3st4 3 un4 pr0v4",
-"manager" : 1
+"manager" : 2
 }""";
 
 expected_response = json.loads("""
@@ -14,7 +14,7 @@ expected_response = json.loads("""
 """)
 
 def exec_test(debug=False):
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Cookie" : "_pdt_session_id_="+login.cookie_id+""}
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Cookie" : "_pdt_session_id_="+login_sh.cookie_id+""}
     conn = http.client.HTTPConnection("localhost", 8080)
     conn.request("POST", "/projects/create", params, headers)
 
