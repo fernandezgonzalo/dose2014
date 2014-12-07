19f57a9f6309dd72793ff605ceefbc8e7f81dc56
buurentriko@gmail.com <buurentriko@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-07 00:55:28 +0000

    fix create user with email registered, add tests

--- trunk/implementations/group1/tests/test_api_rest.py
+++ trunk/implementations/group1/tests/test_api_rest.py
@@ -27,42 +27,42 @@ def post_users(session, name, lastname, email, password, rol, active):
 	uri_post_users = "http://localhost:9090/api/users"
 	payload = {"name": name, "lastname": lastname, "email": email, "password": password, "rol": rol, "active": active}
 	headers = {"content-type": "application/json"}
-	return s.post(uri_post_users, data=json.dumps(payload), headers=headers)
+	return session.post(uri_post_users, data=json.dumps(payload), headers=headers)
 
 def delete_users(session, id_user):
 	uri_delete_users = "http://localhost:9090/api/users/%s" % id_user
 	headers = {"content-type": "application/json"}
-	return s.delete(uri_delete_users, headers=headers)
+	return session.delete(uri_delete_users, headers=headers)
 
 def put_users(session, id_user, name, lastname, password, rol, active):
 	uri_put_users = "http://localhost:9090/api/users/%s" % id_user
 	payload = {"name": name, "lastname": lastname, "password":password, "rol": rol, "active": active}
 	headers = {"content-type": "application/json", "accept": "applicacion/json"}
-	return s.put(uri_put_users, data=json.dumps(payload), headers=headers)
+	return session.put(uri_put_users, data=json.dumps(payload), headers=headers)
 
 # PROJECTS
 
 def get_projects(session, id_user):
 	uri_get_projects = "http://localhost:9090/api/users/%s/projects" % id_user
 	headers = {"content-type": "application/json"}
-	return s.get(uri_get_projects, headers=headers)
+	return session.get(uri_get_projects, headers=headers)
 
 def get_project(session, id_project):
 	uri_get_project = "http://localhost:9090/api/users/projects/%s" % id_project
 	headers = {"content-type": "application/json"}
-	return s.get(uri_get_project, headers=headers)
+	return session.get(uri_get_project, headers=headers)
 
 def add_project(session, id_user, name, info):
 	uri_add_project = "http://localhost:9090/api/users/%s/projects" % id_user
 	payload = {"name": name, "info": info}
 	headers = {"content-type": "application/json"}
-	return s.post(uri_add_project, data=json.dumps(payload), headers=headers)
+	return session.post(uri_add_project, data=json.dumps(payload), headers=headers)
 
 def update_project(session, id_project, name, info):
 	uri_update_project = "http://localhost:9090/api/users/projects/%s" % id_project
 	payload = {"name": name, "info": info}
 	headers = {"content-type": "application/json"}
-	return s.put(uri_update_project, data=json.dumps(payload), headers=headers)
+	return session.put(uri_update_project, data=json.dumps(payload), headers=headers)
 
 if __name__ == '__main__':
 	s = requests.Session()
