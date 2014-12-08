f1b41da4501cc3843d9e0dfe54f4776176648da3
payomaty666@gmail.com <payomaty666@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-08 15:58:23 +0000

    [Group 6][Rio Cuarto6] [Front-End] Update login page

--- trunk/implementations/group6/www/js/sessionCtrl.js
+++ trunk/implementations/group6/www/js/sessionCtrl.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('Wbpms')
-  .controller('SessionCtrl', ['$scope', '$http', '$log', 'UserData',
+  .controller('SessionCtrl', ['$scope', '$http', '$log', 'UserData', //'$upload',
     function ($scope, $http, $log, UserData) {
-        
+
         $scope.loginModel = UserData;
 
         $scope.newUser = {
@@ -13,7 +13,8 @@ angular.module('Wbpms')
             new_user_name : '',
             new_user_surname : '',
             new_user_gender : '',
-            new_user_role : ''
+            new_user_role : '',
+            new_user_avatar: ''
         };
         
         $scope.roles = {  
@@ -39,9 +40,8 @@ angular.module('Wbpms')
         // declaration !AND! call (see parenthesis at end of function)
         // of a function that fetches the todos from the server
         $scope.init = function() {
-            if($scope.loggedUser){
-                window.location.href = '#/home';  
-            }
+            if(scope.loginModel.email != '')
+                window.location.href = '#/home';
         };
 
         // Function login user.
@@ -54,35 +54,92 @@ angular.module('Wbpms')
             $scope.newUser.new_user_gender = '';
             $scope.newUser.new_user_role = '';
         }
+
+        $scope.checkLogIn = function(_loginModel){
+            if(_loginModel.email != null && _loginModel.email !== '' && _loginModel.password != null && _loginModel.password !== '' && _loginModel.password.length >= 8)
+                return true;
+            else
+                return false;
+        }
     
-        $scope.logIn = function(_loginModel) {
-            // construct the payload that we will send as part of the post request
-            var payload = {
-                email : _loginModel.email,
-                password : _loginModel.password
-            };
-            $log.debug("Sending payload: " + JSON.stringify(payload));
-            // send the payload to the server
-            $http.post('/api/sessions', payload)
+/*        $scope.uploadFile = function(file) {
+            var fd = new FormData();
+            //Take the first selected file
+            fd.append("file", file[0]);
+
+            $http.post('', fd, {
+                withCredentials: true,
+                headers: {'Content-Type': undefined },
+                transformRequest: angular.identity
+            })
             .success(function(data, status, header, config) {
-                $log.debug('Success login user');
-                $scope.loginModel = data.success;
-                $scope.logInSuccessMsgVisible = true;
-                $scope.logInErrorMsgVisible = false;
-                //Sign Up info messagges //
-                $scope.signUpSuccessMsgVisible = false;
-                $scope.signUpErrorMsgVisible = false;
-                //Log Out info messagges //
-                $scope.logOutSuccessMsgVisible = false;
-                $scope.logOutErrorMsgVisible = false;
-                //Forget Password info messagges //
-                $scope.forgetPasswordSuccessMsgVisible = false;
-                $scope.forgetPasswordErrorMsgVisible = false;
-                $scope.loggedUser = true;
-                window.location.href = '#/home';
+                $log.debug('Success file upload');                    
+                $scope.loginModel.avatarpath = data.avatarpath;
             })
             .error(function(data, status) {
-                $log.debug('Error while trying login user');
+                $log.debug('Error while trying upload file');
+            });
+        };*/
+
+        $scope.logIn = function(_loginModel) {
+            // construct the payload that we will send as part of the post request
+            if($scope.checkLogIn(_loginModel)){
+                var payload = {
+                    email : _loginModel.email,
+                    password : _loginModel.password
+                };
+                $log.debug("Sending payload: " + JSON.stringify(payload));
+                // send the payload to the server
+                $http.post('/api/sessions', payload)
+                .success(function(data, status, header, config) {
+                    $log.debug('Success login user');
+                    
+                    $scope.loginModel.name = data.name;
+                    $scope.loginModel.surname = data.surname;
+                    $scope.loginModel.email = data.email;
+                    $scope.loginModel.password = data.password;
+                    $scope.loginModel.role = data.role;
+                    $scope.loginModel.gender = data.gender;
+                    $scope.loginModel.changepwd = data.changepwd;
+
+/*                    if(data.avatarpath===''){
+                        if(data.gender === 'Male')
+                            $scope.loginModel.avatar = 'img/male.png';
+                        else
+                            $scope.loginModel.avatar = 'img/female.png';
+                    }else{
+                        $scope.loginModel.avatar = data.avatarpath;
+                    }*/
+                    
+                    $scope.logInSuccessMsgVisible = true;
+                    $scope.logInErrorMsgVisible = false;
+                    //Sign Up info messagges //
+                    $scope.signUpSuccessMsgVisible = false;
+                    $scope.signUpErrorMsgVisible = false;
+                    //Log Out info messagges //
+                    $scope.logOutSuccessMsgVisible = false;
+                    $scope.logOutErrorMsgVisible = false;
+                    //Forget Password info messagges //
+                    $scope.forgetPasswordSuccessMsgVisible = false;
+                    $scope.forgetPasswordErrorMsgVisible = false;
+                    $scope.loggedUser = true;
+                    window.location.href = '#/home';
+                })
+                .error(function(data, status) {
+                    $log.debug('Error while trying login user');
+                    $scope.logInSuccessMsgVisible = false;
+                    $scope.logInErrorMsgVisible = true;
+                    //Sign Up info messagges //
+                    $scope.signUpSuccessMsgVisible = false;
+                    $scope.signUpErrorMsgVisible = false;
+                    //Log Out info messagges //
+                    $scope.logOutSuccessMsgVisible = false;
+                    $scope.logOutErrorMsgVisible = false;
+                    //Forget Password info messagges //
+                    $scope.forgetPasswordSuccessMsgVisible = false;
+                    $scope.forgetPasswordErrorMsgVisible = false;
+                });
+            }else{
                 $scope.logInSuccessMsgVisible = false;
                 $scope.logInErrorMsgVisible = true;
                 //Sign Up info messagges //
@@ -93,40 +150,61 @@ angular.module('Wbpms')
                 $scope.logOutErrorMsgVisible = false;
                 //Forget Password info messagges //
                 $scope.forgetPasswordSuccessMsgVisible = false;
-                $scope.forgetPasswordErrorMsgVisible = false;
-            });
+                $scope.forgetPasswordErrorMsgVisible = false;   
+            }
        }
 
+        $scope.checkSingUp = function(_newUser){
+            if(_newUser.new_user_email !== '' && _newUser.new_user_password !== '' && _newUser.new_user_password.length >= 8
+                && _newUser.new_user_confirm_password === _newUser.new_user_password && _newUser.new_user_name !== '' && _newUser.new_user_surname !== '' && _newUser.new_user_gender !== '')
+                return true;
+            else
+                return false;
+        }
+
        $scope.singUp = function(_newUser) {
-       // construct the payload that we will send as part of the post request
-            var payload = {
-                email : _newUser.new_user_email,
-                password : _newUser.new_user_password,
-                name : _newUser.new_user_name,
-                surname : _newUser.new_user_surname,
-                gender : _newUser.new_user_gender,
-                role : _newUser.new_user_role,
-                photo : 'img/users_avatars/'+_newUser.new_user_username+'.png'
-            }
-            $log.debug("Sending payload: " + JSON.stringify(payload));
-            // send the payload to the server
-            $http.post('/api/users', payload)
-            .success(function(data, status, header, config) {
-                $log.debug('Success adding new user');
-                $scope.logInSuccessMsgVisible = false;
-                $scope.logInErrorMsgVisible = false;
-                //Sign Up info messagges //
-                $scope.signUpSuccessMsgVisible = true;
-                $scope.signUpErrorMsgVisible = false;
-                //Log Out info messagges //
-                $scope.logOutSuccessMsgVisible = false;
-                $scope.logOutErrorMsgVisible = false;
-                //Forget Password info messagges //
-                $scope.forgetPasswordSuccessMsgVisible = false;
-                $scope.forgetPasswordErrorMsgVisible = false;
-            })
-            .error(function(data, status) {
-                $log.debug('Error while trying to add new user');
+            if($scope.checkSingUp(_newUser)){
+                var payload = {
+                    email : _newUser.new_user_email,
+                    password : _newUser.new_user_password,
+                    name : _newUser.new_user_name,
+                    surname : _newUser.new_user_surname,
+                    gender : _newUser.new_user_gender,
+                    role : _newUser.new_user_role,
+                    photo : 'img/users_avatars/'+_newUser.new_user_username+'.png'
+                }
+                $log.debug("Sending payload: " + JSON.stringify(payload));
+                // send the payload to the server
+                $http.post('/api/users', payload)
+                .success(function(data, status, header, config) {
+                    $log.debug('Success adding new user');
+                    $scope.logInSuccessMsgVisible = false;
+                    $scope.logInErrorMsgVisible = false;
+                    //Sign Up info messagges //
+                    $scope.signUpSuccessMsgVisible = true;
+                    $scope.signUpErrorMsgVisible = false;
+                    //Log Out info messagges //
+                    $scope.logOutSuccessMsgVisible = false;
+                    $scope.logOutErrorMsgVisible = false;
+                    //Forget Password info messagges //
+                    $scope.forgetPasswordSuccessMsgVisible = false;
+                    $scope.forgetPasswordErrorMsgVisible = false;
+                })
+                .error(function(data, status) {
+                    $log.debug('Error while trying to add new user');
+                    $scope.logInSuccessMsgVisible = false;
+                    $scope.logInErrorMsgVisible = false;
+                    //Sign Up info messagges //
+                    $scope.signUpSuccessMsgVisible = false;
+                    $scope.signUpErrorMsgVisible = true;
+                    //Log Out info messagges //
+                    $scope.logOutSuccessMsgVisible = false;
+                    $scope.logOutErrorMsgVisible = false;
+                    //Forget Password info messagges //
+                    $scope.forgetPasswordSuccessMsgVisible = false;
+                    $scope.forgetPasswordErrorMsgVisible = false;
+                });
+            }else{
                 $scope.logInSuccessMsgVisible = false;
                 $scope.logInErrorMsgVisible = false;
                 //Sign Up info messagges //
@@ -138,7 +216,7 @@ angular.module('Wbpms')
                 //Forget Password info messagges //
                 $scope.forgetPasswordSuccessMsgVisible = false;
                 $scope.forgetPasswordErrorMsgVisible = false;
-            });
+            }
        }
        
        $scope.logout = function() {
