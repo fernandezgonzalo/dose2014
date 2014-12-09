91996c3ffd8794f04bbcd77f7dd4fda5da496895
jorgeraulpadula@gmail.com <jorgeraulpadula@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-09 16:24:42 +0000

    edit user

--- implementations/group2/www/app/users/controllers/users.controller.js
+++ implementations/group2/www/app/users/controllers/users.controller.js
@@ -15,12 +15,39 @@ angular.module('coffee.users').controller('LoginCtrl', ['$scope', '$rootScope', 
     }
 ]).controller('RegisterCtrl', ['$scope', '$rootScope', '$http', '$location', 'Users', 'Global',
     function($scope, $rootScope, $http, $location, Users, Global) {
+        $scope.initEdit = function() {
+            $scope.user = Global.user;
+        };
+        $scope.edit = function() {
+            console.log(this.user)
+            var u = {}
+            if (this.user.new_password == this.user.new_password_repeated) {
+                
+                
+                var id = this.user.id;
+                var self = this;
+                Users.one(id).get().then(function(user) {
+                    var u = user;
+                    u.first_name = self.user.first_name;
+                    u.last_name = self.user.last_name;
+                    u.password = self.user.new_password;
+
+                    u.put().then(function(){
+                        console.log("SUCCESS")
+                    });
+                });
+
+            } else {
+                $scope.error = "Error, different passwords"
+            }
+
+        };
         $scope.user = {};
         $scope.register = function() {
             Users.post($scope.user).then(function(user) {
                 $location.url('/');
             }, function(err) {
-                $scope.error = response.Message;
+                $scope.error = err.Message;
             });
         };
     }
