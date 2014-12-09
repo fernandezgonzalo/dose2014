dba54c7eaa66f7dc583984460e906e7e9618d978
jorgeraulpadula@gmail.com <jorgeraulpadula@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-09 20:22:44 +0000

    add/remove user from a project

--- trunk/implementations/group2/www/app/core/controllers/project_users.controller.js
+++ trunk/implementations/group2/www/app/core/controllers/project_users.controller.js
@@ -29,7 +29,23 @@ angular.module('coffee.core').controller('ProjectUsersController', ['$scope', '$
         $scope.findAllUsers = function() {
             Users.getList().then(function(users) {
                 $scope.users = users;
+                $scope.users = userToAdd();
             });
+        };
+
+        var userToAdd = function() {
+            var updatedUsers = [];
+            for (var i = $scope.users.length - 1; i >= 0; i--) {
+                var flag = false;
+                for (var j = $scope.current_users.length - 1; j >= 0; j--) {
+                    if ($scope.current_users[j].id == $scope.users[i].id) {
+                        flag = true;
+                    }
+                };
+                if (!flag)
+                    updatedUsers.push($scope.users[i]);
+            };
+            return updatedUsers;
         };
 
         $scope.addUser = function(user) {
@@ -37,6 +53,9 @@ angular.module('coffee.core').controller('ProjectUsersController', ['$scope', '$
 
             Users.one(user.id).all('projects').one(project_id).post().then(function(response) {
                 $scope.current_users.push(user);
+                
+
+                $scope.users = userToAdd();
             });
         };
 
@@ -46,6 +65,7 @@ angular.module('coffee.core').controller('ProjectUsersController', ['$scope', '$
             Users.one(user.id).all('projects').one(project_id).remove().then(function(response) {
                 for (var i in $scope.current_users) {
                     if ($scope.current_users[i].id === user.id) {
+                        $scope.users.push($scope.current_users[i]);
                         $scope.current_users.splice(i, 1);
                     }
                 }
