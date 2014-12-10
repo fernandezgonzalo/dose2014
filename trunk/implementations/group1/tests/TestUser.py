dac95b0ff106c1dd5671420fbd6f5b8a0c5906a4
g.guidogiovannini.g@gmail.com <g.guidogiovannini.g@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-10 04:19:12 +0000

    Group1: modified user tests

--- trunk/implementations/group1/tests/TestUser.py
+++ trunk/implementations/group1/tests/TestUser.py
@@ -55,11 +55,26 @@ class TestUser(unittest.TestCase):
         self.assertEqual(req.status_code, 401)
         self.assertEqual(req.json().get('Message'), "Email already registered")
 
+
+	
+
     def test_full(self):
+
+	### things that are tested:
+	### 1) get all users
+	### 2) insert a new test user
+	### 3) verify that there is now a more user in the database
+	### 4) verify that the data entered correctly
+	### 5) modify user data
+	### 6) verify that the data is successfully modified
+	### 7) get all users
+	### 8) delete test user
+	### 9) verify that an item was removed in the database
+	
 
 	do_login(self.session, "dex@asd.com", "blood")
 	
-		# get all users
+		# get all users in users_1
 	users_1 = json.loads(get_users(self.session).content)
 
         name = "test_name"
@@ -72,7 +87,7 @@ class TestUser(unittest.TestCase):
         req = post_users(self.session, name, lastname, email, password, rol, active)
 	self.assertEqual(req.status_code, 200)
 	
-		# get all users
+		# get all users in users_2
 	users_2  = json.loads(get_users(self.session).content)
 
 		# the number of users is increased by 1 after inserting a user
@@ -102,11 +117,11 @@ class TestUser(unittest.TestCase):
 	self.assertEqual(user_info_2 ['email'], "test@email.com")
 	self.assertEqual(user_info_2 ['password'], "test_password_2")
 
-		#get all users
+		#get all users in users_1
 	users_1 = json.loads(get_users(self.session).content)
 		# delete user
 	req2 = delete_users (self.session, id_user)
-		#get all users
+		#get all users in users_2
 	users_2  = json.loads(get_users(self.session).content)
 		# the number of users is decremented by 1 after deleting a user
 		# the length users_1 is equal to the length (users_2 + 1) 		
