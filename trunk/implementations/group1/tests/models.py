1777bff65f4b072846fec1a115212ccb2e611768
buurentriko@gmail.com <buurentriko@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-10 14:59:29 +0000

    add orm for testing

---
+++ trunk/implementations/group1/tests/models.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+
+from peewee import *
+import json
+
+database_proxy = Proxy()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database_proxy
+
+class Project(BaseModel):
+    info = TextField(null=True)
+    name = TextField(null=True)
+
+    class Meta:
+        db_table = 'Project'
+
+    def jsonify(self):
+        return json.dumps({
+            "info": self.info,
+            "name": self.name,
+            "id": str(self.id)
+            })
+
+class Requirement(BaseModel):
+    desc = TextField(null=True)
+    estimation = IntegerField(null=True)
+    id_project = IntegerField(null=True)
+
+    class Meta:
+        db_table = 'Requirement'
+
+    def jsonify(self):
+        return json.dumps({
+            "desc": self.desc,
+            "estimation": str(self.estimation),
+            "id_project": str(self.id_project),
+            "id": str(self.id)
+            })
+
+class User(BaseModel):
+    active = IntegerField(null=True)
+    email = TextField(null=True)
+    lastname = TextField(null=True)
+    name = TextField(null=True)
+    password = TextField(null=True)
+    rol = IntegerField(null=True)
+
+    class Meta:
+        db_table = 'User'
+
+    def jsonify(self):
+        return json.dumps({
+            "active": str(self.active),
+            "email": self.email,
+            "lastname": self.lastname,
+            "name": self.name,
+            "password": self.password,
+            "rol": str(self.rol),
+            "id": str(self.id)
+            })
+
+class Rolproject(BaseModel):
+    type = TextField(null=True)
+
+    class Meta:
+        db_table = 'RolProject'
+
+    def jsonify(self):
+        return json.dumps({
+            "type": self.type,
+            "id": str(self.id)
+            })
+
+class RolprojectUserproject(BaseModel):
+    id_project = ForeignKeyField(Project, null=True)
+    id_rolproject = IntegerField(null=True)
+    id_user = ForeignKeyField(User, null=True)
+
+    class Meta:
+        db_table = 'RolProject_UserProject'
+
+    def jsonify(self):
+        return json.dumps({
+            "id_project": str(self.id_project),
+            "id_rolproject": str(self.id_rolproject),
+            "id_user": str(self.id_user)
+            })
+
+class Sprint(BaseModel):
+    duration = IntegerField(null=True)
+    id_project = IntegerField(null=True)
+
+    class Meta:
+        db_table = 'Sprint'
+
+    def jsonify(self):
+        return json.dumps({
+            "duration": str(self.duration),
+            "id_project": str(self.id_project),
+            "id": str(self.id)
+            })
+
+class Task(BaseModel):
+    comment = TextField(null=True)
+    desc = TextField(null=True)
+    duration = IntegerField(null=True)
+    id_requirement = IntegerField(null=True)
+    id_user = IntegerField(null=True)
+    points = IntegerField(null=True)
+    status = TextField(null=True)
+
+    class Meta:
+        db_table = 'Task'
+
+    def jsonify(self):
+        return json.dumps({
+            "comment": self.comment,
+            "desc": self.desc,
+            "duration": str(self.duration),
+            "id_requirement": str(self.id_requirement),
+            "id_user": str(self.id_user),
+            "points": str(self.points),
+            "status": self.status,
+            "id": str(self.id)
+            })
+
+class TaskuserSprint(BaseModel):
+    id_sprint = IntegerField(null=True)
+    id_task = ForeignKeyField(Task,null=True)
+    id_user = ForeignKeyField(User, null=True)
+
+    class Meta:
+        db_table = 'TaskUser_Sprint'
+
+    def jsonify(self):
+        return json.dumps({
+            "id_sprint": str(self.id_sprint),
+            "id_task": str(self.id_task),
+            "id_user": str(self.id_user)
+            })
+
+class Userproject(BaseModel):
+    id_project = ForeignKeyField(Project, null=True)
+    id_user = ForeignKeyField(User, null=True)
+
+    class Meta:
+        db_table = 'UserProject'
+
+    def jsonify(self):
+        return json.dumps({
+            "id_project": str(self.id_project),
+            "id_user": str(self.id_user)
+            })
---
+++ trunk/implementations/group1/tests/test.py
@@ -0,0 +1,35 @@
+import requests
+import unittest
+from test_api_rest import *
+from models import *
+from faker import Factory
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.session = requests.Session()
+        self.faker = Factory.create()
+        self.database = SqliteDatabase('../database.db')
+        database_proxy.initialize(self.database)
+        do_login(self.session, "bob@esponja.com", "asd")
+
+    def test_create_user(self):
+        name = self.faker.first_name()
+        lastname = self.faker.last_name()
+        email = self.faker.email()
+        password = self.faker.password()
+        rol = "1"
+        active = "1"
+
+        res = post_users(self.session, name, lastname, email, password, rol, active)
+        self.assertEqual(res.status_code, 200)
+        u = User.get(User.id == res.json().get('id'))
+        self.assertEqual(u.email, email)
+
+    def test_update_user(self):
+        new_name = "jaja"
+        user = get_user(self.session, 39).json()
+        res = put_users(self.session, 39, new_name, user.get("lastname"), user.get("email"), "1", "1")
+        #user = get_user(self.session, 39).json()
+        user = User.get(User.id == 39)
+        self.assertEqual(user.name, new_name)
