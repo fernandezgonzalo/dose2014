a0b1fb79192108ea7d4e617d50a8fd2ce4936af5
gabriele.madd@gmail.com <gabriele.madd@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-11 20:56:40 +0000

    Group5: some code refactoring and bug fixes

--- implementations/group5/frontend/app/js/services/task.js
+++ implementations/group5/frontend/app/js/services/task.js
@@ -7,17 +7,19 @@ angular.module('Mgmt').factory('Task', ['$resource', '$log', function($resource,
 
   return $resource('/api/tasks/:taskId', {taskId: '@id'},
                    {
-                     'update':          {method: 'PUT'},
-                     'getProjectTasks': {method: 'GET',
-                                         params: {projectId: '@id_project'}, 
-                                         url: '/api/projects/:projectId/tasks',
-                                         isArray: true},
-                     'getUserTasks': { method: 'GET',
+                   	'update':          	{method: 'PUT'},
+                   	'delete': 			{method: 'DELETE',
+                   						params: {taskId: '@id', projectId: '@id_project'},
+                   						url: '/api/tasks/:taskId'},
+                    'getProjectTasks': 	{method: 'GET',
+                                        params: {projectId: '@id_project'}, 
+                                        url: '/api/projects/:projectId/tasks',
+                                        isArray: true},
+                    'getUserTasks': 	{method: 'GET',
                      					params: {userId: '@id_user_assigned'},
                      					url: '/api/users/:userId/tasks',
-                     					isArray: true
-                     }
-                   });
+                     					isArray: true}
+  });
   
 
 }]);
