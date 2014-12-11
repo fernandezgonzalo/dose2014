856499f6a7169fde7dca960f2a6153454e0529df
eli.adova@abv.bg <eli.adova@abv.bg@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-11 01:18:28 +0000

    Adding controller for managing users - NOT finished.

--- implementations/group1/www/js/factories/usersFromProjectFactory.js
+++ implementations/group1/www/js/factories/usersFromProjectFactory.js
@@ -1,3 +1,10 @@
-	angular.module('DOSEMS.services').factory('UsersFromProject', function ($resource) {
+angular.module('DOSEMS.services').factory('UsersFromProject', function ($resource) {
     return $resource('api/users/:userId/projects/:projectId/developers', {}, {get: {method: 'get', isArray: true}});
 });
+angular.module('DOSEMS.services').factory('RemoveUserFromProject', function ($resource) {
+	return $resource('/api/users/:userId/projects/:projectId/developers/:developerId', {}, 
+		{ 
+			'remove': {method: 'DELETE'}
+		}); 
+});
+
