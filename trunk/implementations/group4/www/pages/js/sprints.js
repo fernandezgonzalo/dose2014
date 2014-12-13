21d593fc7970d513a34b33626728d8de612ccc69
dimykus@gmail.com <dimykus@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-13 12:56:46 +0000

    add sprints page

---
+++ trunk/implementations/group4/www/pages/js/sprints.js
@@ -0,0 +1,58 @@
+define(
+    [
+        //System includes
+        "angular",
+
+        //Custom includes
+        "pages/js/restapi"
+    ],
+    function(angular)
+    {
+        return angular.module
+        (
+            "SprintsModule",
+            [
+                "RestApiModule"
+            ]
+        )
+            .controller
+        (
+            "SprintsCtr",
+            [
+                "$scope",
+                "$log",
+                "restapi",
+                "sprints",
+                "projects",
+                "$state",
+                function($scope, $log, restapi, sprints, projects, $state)
+                {
+                    $scope.sprints = sprints;
+                    $scope.projects = projects;
+                    $scope.state = $state;
+
+                    $scope.go = function(project_id, sprint_id)
+                    {
+                        $state.go("project.sprint", {id:project_id, sprint_id:sprint_id} );
+                    }
+                }
+            ]
+        )
+            .factory
+    (
+        'SprintsProvider',
+        [
+            "restapi",
+            function(restapi)
+            {
+                var module = {};
+                module.resolver = function()
+                {
+                    return restapi.sprints();
+                };
+                return module;
+            }
+        ]
+    )
+    }
+);
