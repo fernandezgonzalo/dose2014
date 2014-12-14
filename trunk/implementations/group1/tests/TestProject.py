50f41c6e76bcd74da9e6a6073a419c0e5eccfd0e
buurentriko@gmail.com <buurentriko@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-14 23:24:02 +0000

    add test

--- trunk/implementations/group1/tests/TestProject.py
+++ trunk/implementations/group1/tests/TestProject.py
@@ -2,74 +2,32 @@ import unittest
 import requests
 import json
 from test_api_rest import *
+from models import *
 from faker import Factory
 
 class TestProject(unittest.TestCase):
 
     def setUp(self):
         self.session = requests.Session()
-
-    '''def test_full(self):
-
-	### things that are tested:
-	### 1) get all projects
-	### 2) insert a new test project
-	### 3) verify that there is now a more project in the database
-	### 4) verify that the data entered correctly
-	### 5) modify project data
-	### 6) verify that the data is successfully modified
-	### 7) get all projects
-	### 8) delete test project
-	### 9) verify that an item was removed in the database
+        self.faker = Factory.create()
+        self.database = SqliteDatabase('../database.db')
+        database_proxy.initialize(self.database)
+        self.res_login = do_login(self.session, "bob_esponja@gmail.com", "asd")
 
 
-	do_login(self.session, "dex@asd.com", "blood")
+    def test_create_project(self):
+    	name = self.faker.word()
+    	info = self.faker.sentence()
 
-		# get all projects in projects_1
-	projects_1 = json.loads(get_projects(self.session, 5).content)
+    	res = add_project(self.session, self.res_login.json().get('id'), name, info)
+    	self.assertEqual(res.status_code, 200, "Status code is not 200")
+    	self.assertEqual(res.json().get('Message'), "Added project ", "The message is no 'Added project '")
 
-        name = "test_name"
-        info = "test_info"
-		# insert a user
-        req = add_project(self.session,5, name, info)
-	self.assertEqual(req.status_code, 200)
 
-		# get all projects in projects_2
-	projects_2  = json.loads(get_projects(self.session, 5).content)
+    def test_update_project(self):
 
-		# the number of projects is increased by 1 after inserting a project
-		# the length projects_1 is equal to the length (projects_2 - 1)
-	self.assertEqual(len (projects_1), len  (projects_2)-1)
 
-		# get the id of test project in "id_project"
-	for i in range (len  (projects_2)):
-		if projects_2 [i] ['id_user'] == "5" and projects_2 [i] ['id_project'] == id_project:
-			id_project = projects_2 [i] ['id']
 
-		# obtain project information
-	project_info = json.loads(get_project (self.session, id_project).content)
-		# verify that the data entered correctly
-	self.assertEqual(project_info ['name'], "test_name")
-	self.assertEqual(project_info ['info'], "test_info")
-
-		# modify project data
-	put_project = update_projects(self.session, id_project, "test_name_2", "test_info_2")
-
-		#verify that the data is successfully modified
-	project_info_2 = json.loads(get_project (self.session, id_project).content)
-	self.assertEqual(project_info_2 ['name'], "test_name_2")
-	self.assertEqual(project_info_2 ['info'], "test_info_2")
-
-		#get all projects in projects_1
-	projects_1 = json.loads(get_projects(self.session,5).content)
-		# delete project
-	req2 = delete_project (self.session, id_project)
-		#get all projects in project_2
-	projects_2  = json.loads(get_projects(self.session,5).content)
-		# the number of projects is decremented by 1 after deleting a project
-		# the length projects_1 is equal to the length (projects_2 + 1)
-	self.assertEqual(len (projects_1), len  (projects_2)+1) '''
-	self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
