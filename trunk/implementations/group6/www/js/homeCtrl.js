9b11e74d9e668f05ea6a387bca1254a1d5f02a5a
nico.neg88@gmail.com <nico.neg88@gmail.com@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-16 13:38:28 +0000

    (Grupo 6(Río Cuarto 6))

--- trunk/implementations/group6/www/js/homeCtrl.js
+++ trunk/implementations/group6/www/js/homeCtrl.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('Wbpms')
-  .controller('HomeCtrl', ['$scope', '$http', '$log', 'UserData', 'ProjectData', 'IterationData', 'ViewHomeData',
-    function ($scope, $http, $log, UserData, ProjectData, IterationData, ViewHomeData) {
+  .controller('HomeCtrl', ['$scope', '$http', '$log', 'UserData', 'ProjectData', 'IterationData', 'ViewHomeData', 'SearchMemberData',
+    function ($scope, $http, $log, UserData, ProjectData, IterationData, ViewHomeData, SearchMemberData) {
 	
         //Global User Data
         $scope.usuario = UserData;  
@@ -15,6 +15,8 @@ angular.module('Wbpms')
 
         //Global Search Member Data        
         $scope.searchData = ViewHomeData;
+
+        $scope.memberToShow = SearchMemberData;
 
         $scope.projects = [];    
 
@@ -118,6 +120,13 @@ angular.module('Wbpms')
           $scope.searchData.email = memberEmail;
           $scope.searchData.name = memberName;
           $scope.searchData.surname = memberSurname;
+
+          $scope.memberToShow.name = '';   
+          $scope.memberToShow.surname = '';
+          $scope.memberToShow.email = '';
+          $scope.memberToShow.gender = '';
+          $scope.memberToShow.role = '';
+          $scope.memberToShow.avatar = '';          
 
           window.location.href = '#/projects/members/view';          
     
