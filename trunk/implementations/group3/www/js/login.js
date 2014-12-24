04b863b11947ec2ec34025d9a5dabc2e42ed678a
onponomarev <onponomarev@f6206239-57e0-e8d1-037d-e0d2c07dc7bc>
2014-12-24 00:14:55 +0000

    Login fixed

--- trunk/implementations/group3/www/js/login.js
+++ trunk/implementations/group3/www/js/login.js
@@ -1,91 +1,78 @@
 'use strict';
 
 angular.module('LetsGoTeam')
-  .controller('LoginController', ['$scope', '$http', '$log', '$location', '$timeout',
-    function ($scope, $http, $log,$location, $timeout) {
+    .controller('LoginController', ['$scope', '$http', '$log', '$location', '$timeout',
+        function($scope, $http, $log, $location, $timeout) {
 
-	  $scope.view = 1;
+            $scope.view = 1;
 
-      $scope.data = {};
+            $scope.data = {};
 
-	  $scope.changeView = function (n){
-							$scope.view = n;
-						};
-	  $scope.isView = function (n){
-							return $scope.view === n;
-						};
+            $scope.changeView = function(n) {
+                $scope.view = n;
+            };
+            $scope.isView = function(n) {
+                return $scope.view === n;
+            };
 
-	  // the model that we bind to the input box
-      $scope.user = {
-        email: '',
-		pass: ''
-      };
+            // the model that we bind to the input box
+            $scope.user = {
+                email: '',
+                password: ''
 
-      $scope.successMsgVisible = false;
+            };
 
-      // the function to login with users information
-      $scope.login = function(user) {
+            $scope.successMsgVisible = false;
 
-        $scope.user = user;
+            // the function to login with users information
+            $scope.login = function(user) {
 
-        var i;
-        for (i = 0; i < users.length; i++) {
-          if(users[i].email === $scope.user.email && users[i].password === $scope.user.pass && !logged){
-            logged = true;
-            $scope.successMsgVisible = true;
-            currentUser = users[i];
-            $location.path("/projectsSprints");
-          }
+                $scope.user = user;
+
+                // the payload is simple the json object that we used for binding to the input
+                var payload = $scope.user;
+
+                $http.post('/login', payload)
+                    .success(function(data, status, header, config) {
+                        if (status === 200) {
+                            console.log('Success logging user');
+                            $location.path("/projectsSprints");
+                        } else {
+		            if (!logged)
+                                $scope.changeView(4);
+                            console.log('Incorrect data');
+                        };
+                    })
+                    .error(function(data, status) {
+		        if (!logged)
+			    $scope.changeView(4);
+                        console.log('Error while trying to login');
+		    })
+            };
+
+            $scope.forgotPassword = function(user) {
+
+                var payload = user;
+
+                $http.post('/login/forgot-password', payload)
+                    .success(function(data, status, header, config) {
+
+                        $scope.data = data;
+                        if ($scope.data.status === 'ok') {
+                            $log.debug('New password:' + $scope.status.newPass);
+                        } else {
+                            $log.debug('Incorrect data');
+                        };
+
+                        // reset the todoModel to not have a description (we keep the last selected user)
+                        $scope.user.email = '';
+                        $scope.user.password = '';
+
+                    })
+                    .error(function(data, status) {
+                        $log.debug('Error while trying to get a password');
+                    });
+            }
+
         }
-        if (!logged) $scope.changeView(4);
-        // the payload is simple the json object that we used for binding to the input
-       /* var payload = $scope.user;
-
-        $http.post('/login', payload)
-          .success(function(data, status, header, config) {
-
-            $scope.data = data;
-              if ($scope.data.status === 'ok') {
-                $log.debug('Success logging user');
-              }
-              else{
-                $log.debug('Incorrect data');
-              };
-
-            // reset the todoModel to not have a description (we keep the last selected user)
-            $scope.user.email = '';
-			$scope.user.pass = '';
-
-          })
-          .error(function(data, status) {
-            $log.debug('Error while trying to login');
-          });*/
-      };
-
-      $scope.forgotPassword = function(user){
-
-          var payload = user;
-
-        $http.post('/login/forgot-password', payload)
-            .success(function(data, status, header, config) {
-
-              $scope.data = data;
-              if ($scope.data.status === 'ok') {
-                $log.debug('New password:'+ $scope.status.newPass);
-              }
-              else{
-                $log.debug('Incorrect data');
-              };
-
-              // reset the todoModel to not have a description (we keep the last selected user)
-              $scope.user.email = '';
-              $scope.user.pass = '';
-
-            })
-            .error(function(data, status) {
-              $log.debug('Error while trying to get a password');
-            });
-      }
-
-    } 
-  ]);
+    ]);
